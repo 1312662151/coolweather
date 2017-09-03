d4c61b2cdfb0f6ad6e4691fab05a12c23f0b600e
Liujie <1312662151@qq.com>
2017-09-03 09:04:25 +0800

    成功顯示天氣

---
+++ app/src/main/java/com/example/coolweather/gson/Suggestion.java
@@ -0,0 +1,30 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/9/2.
+ */
+
+public class Suggestion {
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public Carwash carwash;
+
+    public Sport sport;
+
+    public class Comfort{
+        @SerializedName("txt")
+        public String info;
+    }
+    public class Carwash{
+        @SerializedName("txt")
+        public String info;
+    }
+    public class Sport{
+        @SerializedName("txt")
+        public String info;
+    }
+}
